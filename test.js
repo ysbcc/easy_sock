f9acebb0b8733bf10d76482492568b266055e894
zombieyang <zombieyang@tencent.com>
2015-12-03 22:45:22 +0800

    手贱格式化一下代码

--- test.js
+++ test.js
@@ -2,35 +2,35 @@
 
 var EasySock = require("./index.js");
 
-function createSocket(){
+function createSocket() {
 	var easysock = new EasySock();
 	easysock.setConfig({
-		ip : "10.209.6.12",
-		port : 8886,
-		keepAlive : true,
-		timeout : 50
+		ip: "10.209.6.12",
+		port: 8886,
+		keepAlive: true,
+		timeout: 50
 	});
-		
-	easysock.isReceiveComplete = function(packet){
+
+	easysock.isReceiveComplete = function (packet) {
 		if (packet.length < 28)
 			return 0;
 		var Q = packet.readUInt8(0);
 		var A = packet.readUInt8(1);
-		if (Q != 0x51 || A != 0x41) 
-			return -1;			
-		
+		if (Q != 0x51 || A != 0x41)
+			return -1;
+
 		var len = packet.readUInt32BE(4);
 		if (packet.length < len + 28)
 			return 0;
-		return len + 28;				
+		return len + 28;
 	};
-		
-	easysock.encode = function(data, seq){
-		
+
+	easysock.encode = function (data, seq) {
+
 		//do some encode		
 		//The body is 2001,['d0013tdrleb'],[ 'vid',  'title',  'second_title']
-		var body = new Buffer([16,209,15,26,11,100,48,48,49,51,116,100,114,108,101,98,34,3,118,105,100,34,5,116,105,116,108,101,34,12,115,101,99,111,110,100,95,116,105,116,108,101,42,6,110,111,100,101,106,115,50,16,98,48,102,51,48,55,53,100,56,99,98,56,100,55,102,50,72,251,225,196,9,64,1]);
-				
+		var body = new Buffer([16, 209, 15, 26, 11, 100, 48, 48, 49, 51, 116, 100, 114, 108, 101, 98, 34, 3, 118, 105, 100, 34, 5, 116, 105, 116, 108, 101, 34, 12, 115, 101, 99, 111, 110, 100, 95, 116, 105, 116, 108, 101, 42, 6, 110, 111, 100, 101, 106, 115, 50, 16, 98, 48, 102, 51, 48, 55, 53, 100, 56, 99, 98, 56, 100, 55, 102, 50, 72, 251, 225, 196, 9, 64, 1]);
+
 		var packet = new Buffer(20);
 		//包头-start
 		packet.writeInt8(81, 0);	// Q
@@ -45,40 +45,40 @@ function createSocket(){
 		packet = Buffer.concat([packet, body]);
 
 		return packet;
-		
+
 	};
-	
-	easysock.decode = function(packet){
+
+	easysock.decode = function (packet) {
 		//到这里，一定是一个完整的包
 		var seq = packet.readUInt32BE(8);
-		
+
 		var body = packet.slice(32);
 		var msg = {};
-		
+
 		//do some decode
-		
+
 		msg.result = body.toString();
 		msg.seq = seq;
-		
+
 		return msg;
-		
+
 	};
 	return easysock;
 }
 
 var sock = createSocket();
 
-function test(){
-        sock.write({}, function(err, data){
-        if (err){
-            console.log("fail:" + err);
-        }
-        else{
-            console.log("success");
-            console.dir(data);
-        }
+function test() {
+	sock.write({}, function (err, data) {
+		if (err) {
+			console.log("fail:" + err);
+		}
+		else {
+			console.log("success");
+			console.dir(data);
+		}
 
-    });
+	});
 }
 
 test();
